8a7bf1809787de5da7b729c2fb0e109b5fb4a251
siddhantjagdishv <siddhantjagdishv@sahaj.ai>
2024-06-10 16:03:10 +0530

    create programmable die with winning moves

--- test/SnakesAndLaddersTest.java
+++ test/SnakesAndLaddersTest.java
@@ -1,12 +1,63 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class SnakesAndLaddersTest {
 
     private SnakesAndLadders game;
 
     @Test
     void theGameShouldRunAndCompleteSuccessfully(){
-
+        game.run(new ProgrammableDie());
     }
 
+    static class ProgrammableDie implements GameDie{
+        int currentDieIndex = 0;
+        List<Integer> moves = new ArrayList<>(){
+            {
+                add(2);
+                add(6);
+                add(4);
+                add(1);
+                add(6);
+                add(4);
+                add(3);
+                add(4);
+                add(6);
+                add(4);
+                add(3);
+                add(4);
+                add(4);
+                add(4);
+                add(1);
+                add(2);
+                add(5);
+                add(5);
+                add(4);
+                add(6);
+                add(1);
+                add(4);
+                add(2);
+                add(4);
+                add(5);
+                add(2);
+                add(2);
+                add(3);
+                add(3);
+                add(3);
+                add(1);
+                add(3);
+                add(4);
+            }
+        };
+
+        @Override
+        public Integer roll(){
+            if(currentDieIndex >= moves.size())
+                throw new RuntimeException("No Moves Left!");
+            return moves.get(currentDieIndex++);
+
+        }
+    }
 }
