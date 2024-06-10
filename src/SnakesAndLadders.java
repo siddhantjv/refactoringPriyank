95957de57a3d8dc773c9f8a8cc6661bf2c254260
siddhantjagdishv <siddhantjagdishv@sahaj.ai>
2024-06-10 15:17:01 +0530

    Refactor file name

--- src/SnakesAndLadders.java
+++ src/SnakesAndLadders.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-public class Main {
+public class SnakesAndLadders {
 
     public static void main(String[] args) {
         //declare variables
